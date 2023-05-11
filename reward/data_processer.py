f7a4783441ef7738c0986b9ce6fcfe891fd51e32
tk <462304@qq.com>
2023-05-12 01:22:02 +0800

    初始化 chatglm rlhf
    Signed-off-by: tk <462304@qq.com>

---
+++ reward/data_processer.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/4/20 8:32
+import json
+import numpy as np
+from transformers import PreTrainedTokenizer
+
+class CorpusPreprocess_summery:
+    # {
+    #     "info": {
+    #         "id": "t3_2vwp1w",
+    #         "post": "I had a car accident on friday, other party involved was speeding and hit me. but because he denies it it seems like I was wrong because he was supposed to go first under normal circumstances. ( give way road markings ) \n\nbut because it was clear when I checked it I drove on, and when I was almost past the intersection he slammed me in the side near the back seat. and caused me to slide across the road for 2-3 meters hit a street light and then bounce back a meter. both doors completely jammed so i had to climb out the window...\n\ncan I somehow get an investigation going about this to see how fast he had to be driving to get this much force in the collision?\nbecause the damage on my car would suggest that he was driving way faster than the legal limit there. ( which is 50 km/h )\n\nalso another reason why i think he was going way faster than admitted is because he could never have reached the intersection from such a distance as where i could not even see him yet\n\n(pictures of the damage:  ) as you can see with the damage, I am lucky to be alive and unharmed right now... 1ft further forward and it could have been my end...\n\nhelp would be appeciated on this :)",
+    #         "title": "Anybody with knowledge of the Dutch law around ? car accident questions.",
+    #         "subreddit": "legaladvice"
+    #     },
+    #     "summaries": [
+    #         {
+    #             "text": " car accident caused me 2-3m damage to my car both doors totally jammed and driving way faster than usual. need info on what to do with this.. thanks :)",
+    #             "policy": "sup4_ppo_rm3_kl10",
+    #             "note": "Was the accident caused by driving fast."
+    #         },
+    #         {
+    #             "text": " we suspect other party involved of speeding when he hit me but I can't prove it without an investigation into the damage, how can i get such an investigation ? if at all possible.",
+    #             "policy": "ref",
+    #             "note": "Unclear what happened."
+    #         }
+    #     ],
+    #     "choice": 1
+    # }
+    @classmethod
+    def process(cls,tokenizer,lines):
+        D = []
+        for i, line in enumerate(lines):
+            jd = json.loads(line)
+            if not jd:
+                continue
+            info = jd['info']
+            summaries = jd['summaries']
+            choice = jd['choice']
+            if len(summaries) != 2 or choice not in (0, 1):
+                raise ValueError(
+                    f"There should be two summaries with a choice that's either 0 or 1. Received {len(summaries)} summaries and choice={choice}."
+                )
+
+            original_text_field = "post" if info["post"] is not None else "article"
+            text_a = summaries[choice]["text"] + " " + tokenizer.bos_token + " " + info[original_text_field]
+            text_b = summaries[0 if choice == 1 else 1]["text"] + " " + tokenizer.bos_token + " " + info[
+                original_text_field]
+            D.append((text_a, text_b))
+        return D
+
+
+class CorpusPreprocess:
+    # {
+    #     "prompt": "\n\nHuman: I eat too much fast food. What's a way to start cooking at home more?\n\nAssistant: The easiest way to start cooking more at home is to cook a few meals a week and keep those meals varied. Planning meals and having a list ready will make cooking much easier. Shopping once a week will also keep your meals varied. Try cooking a few recipes you've never made before. Maybe you could try some Italian, Mexican, or Asian dishes. Cooking more at home is also a great way to save money. You can buy ingredients in bulk and cook large portions, which will last you several days.\n\nHuman: Tell me about buying in bulk. Will that make the process easier?\n\nAssistant: Buying in bulk will definitely make cooking and planning meals easier. The key to buying in bulk is to only buy what you know you will use. Keeping a well-stocked pantry with a variety of dried and canned goods will make cooking easier. Buying in bulk will save you money and give you greater variety in your meals.\n\nHuman: How does one buy in bulk?\n\nAssistant:",
+    #     "response": " To buy in bulk, start by shopping at warehouse stores or specialty stores. Make a list of the items you usually use in meals and the amounts you use. Make a note of the unit prices for those items. You can then compare unit prices at warehouse stores to see if they're lower. If they are, buy in bulk from warehouse stores. Also, check online for even bigger savings. Another way to buy in bulk is to join a grocery co-op, which is a group of people who buy food in bulk and then split the cost.",
+    #     "chosen": " To buy in bulk, start by shopping at warehouse stores or specialty stores. Make a list of the items you usually use in meals and the amounts you use. Make a note of the unit prices for those items. You can then compare unit prices at warehouse stores to see if they're lower. If they are, buy in bulk from warehouse stores. Also, check online for even bigger savings. Another way to buy in bulk is to join a grocery co-op, which is a group of people who buy food in bulk and then split the cost.",
+    #     "rejected": " Buying in bulk is easy. There are stores in your area that offer bulk food. Organic, bulk foods are also widely available online. You can find organic dried pasta, nuts, baking items, and spices in bulk. When shopping for groceries, buy the largest package or container possible to get the largest savings. Organic, bulk food is more expensive than traditional packaged food, but much cheaper than organic grocery stores. You can get many non-perishable items for half price or less when buying in bulk."
+    # }
+    @classmethod
+    def process(cls,tokenizer,lines):
+        D = []
+        for i, line in enumerate(lines):
+            jd = json.loads(line)
+            if not jd:
+                continue
+            prompt = jd['prompt']
+            # response = jd['response']
+            chosen = jd['chosen']
+            rejected = jd['rejected']
+            text_a = prompt + chosen
+            text_b = prompt + rejected
+            if text_a == text_b:
+                continue
+            D.append((text_a, text_b))
+        return D
+
+class TokenIds:
+    @classmethod
+    def process(cls,pair_data,tokenizer: PreTrainedTokenizer,max_seq_length: int):
+        text_a, text_b = pair_data
+        assert text_a != text_b , ValueError('data must not be same')
+
+        input_ids = tokenizer.encode(text_a, truncation=True, max_length=max_seq_length)
+        input_ids2 = tokenizer.encode(text_b, truncation=True, max_length=max_seq_length)
+
+        ctxlen = input_ids.index(tokenizer.bos_token_id)
+        ctxlen2 = input_ids2.index(tokenizer.bos_token_id)
+
+        input_ids = np.asarray(input_ids,dtype=np.int32)
+        input_ids2 = np.asarray(input_ids2, dtype=np.int32)
+        ctxlen = np.asarray(ctxlen, dtype=np.int32)
+        ctxlen2 = np.asarray(ctxlen2, dtype=np.int32)
+
+        if len(input_ids) == len(input_ids2):
+            if np.all(input_ids == input_ids2):
+                return None
+        return {
+            "input_ids": input_ids,
+            "ctxlen": ctxlen,
+            "input_ids2": input_ids2,
+            "ctxlen2": ctxlen2
+        }
---
+++ reward/data_utils.py
@@ -0,0 +1,162 @@
+# @Time    : 2023/4/19 23:02
+# @Author  : tk
+# @FileName: data_utils
+import sys
+sys.path.append('..')
+
+import copy
+import json
+import os
+import random
+import typing
+import numpy as np
+import torch
+from deep_training.data_helper import DataHelper, ModelArguments, TrainingArguments, DataArguments
+from models import LoraArguments,LoraConfig,ChatGLMTokenizer,ChatGLMConfig
+from fastdatasets.record import load_dataset as Loader, RECORD, WriterObject, gfile
+from transformers import PreTrainedTokenizer, HfArgumentParser
+from data_processer import CorpusPreprocess, TokenIds
+from config.reward_config import *
+from torch.nn import functional as F
+
+def preprocess(text):
+  return text
+
+def postprocess(text):
+  return text
+
+
+class NN_DataHelper(DataHelper):
+    index = 1
+
+    def __init__(self, *args, **kwargs):
+        super(NN_DataHelper, self).__init__(*args, **kwargs)
+
+
+    def on_get_labels(self, files: typing.List[str]):
+        D = ['score']
+        label2id = {label: i for i, label in enumerate(D)}
+        id2label = {i: label for i, label in enumerate(D)}
+        return label2id, id2label
+
+
+    def on_data_ready(self):
+        self.index = -1
+
+    # 切分词
+    def on_data_process(self, data: typing.Any, mode: str):
+        self.index += 1
+
+        tokenizer: PreTrainedTokenizer
+        config = self.config
+        max_seq_length = self.max_seq_length_dict[mode]
+        tokenizer = self.tokenizer
+
+        pair_data = data
+        d = TokenIds.process(pair_data,tokenizer,max_seq_length)
+        if self.index < 3:
+            print(d)
+        return d
+
+    # 读取文件
+    def on_get_corpus(self, files: typing.List, mode: str):
+        tokenizer = self.tokenizer
+        D = []
+        for file in files:
+            with open(file, mode='r', encoding='utf-8', newline='\n') as f:
+                lines = f.readlines()
+            d = CorpusPreprocess.process(tokenizer,lines)
+            D.extend(d)
+        return D
+
+    def collate_fn(self, batch):
+        o = {k: [] for k in batch[0].keys()}
+        for i, b in enumerate(batch):
+            for k in b:
+                o[k].append(torch.tensor(b[k]))
+        max_len = np.max([len(_) for _ in o['input_ids']])
+        flag = False
+        if 'input_ids2' in o:
+            flag = True
+            max_len = np.max([max_len] + [len(_) for _ in o['input_ids2']])
+
+        tokenizer: ChatGLMTokenizer = self.tokenizer
+        pad_val = tokenizer.pad_token_id
+        def get_mask_position_ids(b_input_ids,ctxlens):
+            b_position_ids, b_attention_mask = [], []
+            for input_ids, context_length in zip(b_input_ids, ctxlens):
+                context_length = context_length.squeeze(dim=-1)
+                mask_position = context_length - 1
+                position_ids = list(range(context_length)) + [mask_position] * (max_len - context_length)
+                block_position_ids = [0] * context_length + list(range(1, max_len - context_length + 1))
+
+                attention_mask = torch.ones((1, max_len, max_len))
+                attention_mask = torch.tril(attention_mask)
+                attention_mask[..., :context_length] = 1
+                attention_mask = (attention_mask < 0.5)
+
+                b_position_ids.append(torch.stack((torch.tensor(position_ids), torch.tensor(block_position_ids))))
+                b_attention_mask.append(attention_mask)
+
+            b_attention_mask = torch.stack(b_attention_mask, dim=0)
+            b_position_ids = torch.stack(b_position_ids, dim=0)
+            return b_attention_mask.bool(),b_position_ids.long()
+
+        o['input_ids'] = torch.stack(
+            [F.pad(_, (0, max_len - len(_)), mode='constant', value=pad_val) for _ in o['input_ids']])
+        ctxlens = o.pop('ctxlen')
+        o['attention_mask'],o['position_ids'] = get_mask_position_ids(o['input_ids'],ctxlens)
+        if flag:
+            o['input_ids2'] = torch.stack(
+                [F.pad(_, (0, max_len - len(_)), mode='constant', value=pad_val) for _ in o['input_ids2']])
+            ctxlens2 = o.pop('ctxlen2')
+            o['attention_mask2'], o['position_ids2'] = get_mask_position_ids(o['input_ids2'], ctxlens2)
+        return o
+
+
+
+
+if __name__ == '__main__':
+    parser = HfArgumentParser((ModelArguments, TrainingArguments, DataArguments, LoraArguments))
+    model_args, training_args, data_args, lora_args = parser.parse_dict(train_info_args)
+    lora_args = lora_args.config
+
+    dataHelper = NN_DataHelper(model_args, training_args, data_args)
+    tokenizer, config, _, _ = dataHelper.load_tokenizer_and_config(tokenizer_class_name=ChatGLMTokenizer,
+                                                                   config_class_name=ChatGLMConfig)
+    assert tokenizer.eos_token_id == 130005
+
+
+    # 缓存数据集
+    # 检测是否存在 output/dataset_0-train.record ，不存在则制作数据集
+    if data_args.do_train:
+        dataHelper.make_dataset_with_args(data_args.train_file,mixed_data=False,shuffle=True,mode='train')
+    if data_args.do_eval:
+        dataHelper.make_dataset_with_args(data_args.eval_file, shuffle=False,mode='eval')
+    if data_args.do_test:
+        dataHelper.make_dataset_with_args(data_args.test_file, shuffle=False,mode='test')
+
+
+    # def shuffle_records(record_filenames, outfile, compression_type='GZIP'):
+    #     print('shuffle_records record...')
+    #     options = RECORD.TFRecordOptions(compression_type=compression_type)
+    #     dataset_reader = Loader.RandomDataset(record_filenames, options=options, with_share_memory=True)
+    #     data_size = len(dataset_reader)
+    #     all_example = []
+    #     for i in tqdm(range(data_size), desc='load records'):
+    #         serialized = dataset_reader[i]
+    #         all_example.append(serialized)
+    #     dataset_reader.close()
+    #
+    #     shuffle_idx = list(range(data_size))
+    #     random.shuffle(shuffle_idx)
+    #     writer = WriterObject(outfile, options=options)
+    #     for i in tqdm(shuffle_idx, desc='shuffle record'):
+    #         example = all_example[i]
+    #         writer.write(example)
+    #     writer.close()
+    #
+    #
+    # # 对每个record 再次打乱
+    # for filename in dataHelper.train_files:
+    #     shuffle_records(filename, filename)
