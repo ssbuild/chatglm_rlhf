a6b342b63fc2d51b94f8bb53a91b795842054105
ssbuild <462304@qq.cn>
2023-05-17 08:43:39 +0800

    add bf16 document
    Signed-off-by: ssbuild <462304@qq.cn>

--- rlhf/train.py
+++ rlhf/train.py
@@ -68,8 +68,8 @@ if __name__ == '__main__':
         devices=data_args.devices,
         checkpoint_dir=data_args.output_dir,
         accumulate_grad_batches=training_args.gradient_accumulation_steps,
-        strategy=strategy
-        # precision=16,#半精度
+        strategy=strategy,
+        # precision='bf16',# 混合精度, 需显卡支持
     )
 
     dataHelper = NN_DataHelper(model_args, training_args, data_args,ppo_args=ppo_args)
