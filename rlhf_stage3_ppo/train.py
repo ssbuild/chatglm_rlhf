b08788274d9cbebbe051cb2374064d951dcfd6a1
ssbuild <462304@qq.cn>
2023-09-19 17:11:54 +0800

    update
    Signed-off-by: ssbuild <462304@qq.cn>

--- rlhf_stage3_ppo/train.py
+++ rlhf_stage3_ppo/train.py
@@ -52,7 +52,17 @@ if __name__ == '__main__':
 
     dataHelper.make_dataset_all()
 
-    deepspeed_config = get_deepspeed_config()
+    is_bf16_supported = torch.cuda.is_bf16_supported()
+    # 精度 根据实际情况做调整
+    if is_bf16_supported:
+        precision = 'bf16'
+    else:
+        precision = '16'
+
+    if global_args["quantization_config"] is not None and global_args["quantization_config"].load_in_8bit:
+        precision = "32"
+
+    deepspeed_config = get_deepspeed_config(precision)
     strategy = 'ddp' if torch.cuda.device_count() >= 1 else 'auto'
     if deepspeed_config is not None and len(deepspeed_config):
         strategy = DeepSpeedStrategy(config=deepspeed_config, )
@@ -69,15 +79,6 @@ if __name__ == '__main__':
         training_args=training_args,
         lora_args=lora_args, )
 
-    is_bf16_supported = torch.cuda.is_bf16_supported()
-    # 精度 根据实际情况做调整
-    if is_bf16_supported:
-        precision = 'bf16'
-    else:
-        precision = '16'
-
-    if global_args["quantization_config"] is not None and global_args["quantization_config"].load_in_8bit:
-        precision = "32"
 
     trainer = PPOTrainer(
         callbacks=[ checkpoint_callback],
@@ -97,7 +98,7 @@ if __name__ == '__main__':
 
     if trainer.global_rank == 0:
         #加载lora
-        pl_reward_model = load_reward_model('../stage2_reward/best_ckpt/last')
+        pl_reward_model = load_reward_model('../rlhf_stage2_reward/best_ckpt/last')
 
         #加载 全参数微调或者p-tuning-v2权重
         #pl_reward_model = load_reward_model('../stage2_reward/best_ckpt','../stage2_reward/best_ckpt/last.pt')
